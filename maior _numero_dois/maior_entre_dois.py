b1a36f0462f5b1f54ed04178b42def8e72909956
Thiago0029 <ti.thiagoporto@gmail.com>
2023-04-30 20:35:35 -0300

    imprimindo maior numero

---
+++ maior _numero_dois/maior_entre_dois.py
@@ -0,0 +1,13 @@
+#imprimindo o maior nuemro entre dois números digitados
+
+num1 = float(input("infomre o primeiro número "))
+num2 = float(input("Informe o segundo número "))
+
+if num1 > num2:
+    print(num1)
+
+elif num1 < num2:
+    print(num2)
+
+else:
+    print("Erro")
