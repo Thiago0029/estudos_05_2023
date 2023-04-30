8bf89fc1583911a29978f52d442f684b918dba71
Thiago0029 <ti.thiagoporto@gmail.com>
2023-04-30 19:10:33 -0300

    virificando numero positivo ou negativo

---
+++ positivo_negativo/p_n.py
@@ -0,0 +1,19 @@
+#script para verificação de negativo ou negativo
+
+num = int(input("Informe um numemro negativo ou positivo"))
+
+if (num > 0){
+    print("Positivo")
+}
+
+elif (num < 0){
+    print("Negativo")
+}
+
+elif (num == 0){
+    print("O número é zero")
+}
+
+else {
+    print("Não é um número")
+}
