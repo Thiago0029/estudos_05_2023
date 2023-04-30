9af55bab540a1659ae912ebb1f26436e80f3a9d4
Thiago0029 <ti.thiagoporto@gmail.com>
2023-04-30 20:44:50 -0300

    exibimndo menor numero

---
+++ menor_entre_dois/menor_entre_dois.py
@@ -0,0 +1,13 @@
+#comparando e exibindo o menor numero entre dois numeros
+
+num1 = float(input("Informe primeiro número: "))
+num2 = float(input("Informe segundo número: "))
+
+if num1 > num2:
+    print(num2)
+
+elif num1 < num2:
+    print(num1)
+
+else:
+    print("erro")
