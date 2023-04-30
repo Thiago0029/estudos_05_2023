88f0e68cfd229ec1c4b8ed9bbcc11e8f2780858d
Thiago0029 <ti.thiagoporto@gmail.com>
2023-04-30 19:29:58 -0300

    verificação de idade

---
+++ maior_menor/maior_menor.py
@@ -0,0 +1,12 @@
+#IMaior ou menor de idade
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= 18:
+    print("Maior de idade")
+
+elif idade <18:
+    print("Menor de idade")
+
+else:
+    print("ERRO")
