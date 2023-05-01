4ca62c849b1eaba128c874aa0f86741fd8846244
Thiago0029 <ti.thiagoporto@gmail.com>
2023-05-01 00:28:59 -0300

    soamndo elementos de listas

---
+++ soma_lista_numerica/soma_numeros_lista.py
@@ -0,0 +1,14 @@
+# somando os valores de uma lista numerica
+
+op = 'c'
+soma = []
+i = 0
+
+while op =='c' or op =='C':
+    soma = int(input("Infomre um numero: "))
+    op = input("digite c para continuar: ")
+
+for i in soma:
+    soma =+ i
+
+print(soma)
